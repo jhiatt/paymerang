d1def8b02731d7e30cac09de89c0b96cc55369fd
jhiatt <jordanhiatt@JordansWorkbook.fios-router.home>
2021-01-09 13:31:30 -0500

    working on charts

--- total.js
+++ total.js
@@ -3,31 +3,46 @@ function addTotal(array) {
 
     // loop through each payment
     for (var i = 0; i<array.length; i++) {
-        // console.log(i)
+        
         // save the payment to a variable
         var remittances = array[i].payment.remittance;
 
         var total_amount = 0
-        // loop through remittances
-        if (remittances.length) {
 
+        // ensure the remittance is formatted as an array that can be looped through
+        if (remittances.length) {
+            
+            // loop through remittances.  Calculate and save the total under total_amount
             for (var j = 0; j<remittances.length; j++) {
                 var amount = remittances[j].amount;
-                
+
                 total_amount += amount
-                
-                // console.log(total_amount)
             }
+        // if formatted as a single value and not an array, a loop is not needed and will break.  We do this instead.
         } else {
             total_amount = remittances.amount
-            // console.log(total_amount)
         }
 
-        array[i].payment.total_amount = total_amount;
-        // console.log(array)
+        // save the resulting total_amount to the json array
+        array[i].payment.total_amount = total_amount;   
     }
-
+    // return the array with the total_amounts added
     return array
+}
+
+// we will use this function to display the kendo charts
+function displayCharts(json){
+    $("#chart").kendoChart({
+        title: {
+            text: "Kendo Chart Example"
+        },
+        series: [
+            { name: "Example Series", data: [200, 450, 300, 125] }
+        ],
+        categoryAxis:{
+            categories: [ 2000, 2001, 2002, 2003 ]
+        }
+    });
 }
 
 
@@ -37,13 +52,8 @@ fetch("./invoices.json")
        return response.json();
     })
     // .then(data => addTotal(data));
-    .then(data => console.log(addTotal(data)))
-
-// var newjson = fetch("./invoices.json")
-//     .then(response => {
-//        return response.json();
-//     })
-//     // .then(data => addTotal(data));
-//     .then(data => addTotal(data))
-
-// console.log(newjson)
+    .then(data => addTotal(data))
+    // add in the kendo charts here for question 2
+    .then(res => {
+        displayCharts(res)
+    })  
